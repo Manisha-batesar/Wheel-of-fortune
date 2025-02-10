45effcec583941d72201d882d4d77e28fea4f783
Manisha-batesar <manubatesar@gmail.com>
2025-02-10 16:50:59 +0530

    initialized repo

---
+++ script.js
@@ -0,0 +1,6 @@
+// document.querySelector('.spinbtn').addEventListener('click', function () {
+//   const wheel = document.querySelector('.wheel');
+//   const randomDeg = Math.floor(Math.random() * 360) + 3600; // Random degree for multiple spins
+//   wheel.style.transition = "transform 3s ease-out";
+//   wheel.style.transform = `rotate(${randomDeg}deg)`; // Apply rotation
+// });
