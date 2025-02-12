dbbe0f4b4683711fcd561bd84a448a69b76c5074
Manisha-batesar <manubatesar@gmail.com>
2025-02-12 18:05:44 +0530

    feature: initial wheel by manu

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
-// document.querySelector('.spinbtn').addEventListener('click', function () {
-//   const wheel = document.querySelector('.wheel');
-//   const randomDeg = Math.floor(Math.random() * 360) + 3600; // Random degree for multiple spins
-//   wheel.style.transition = "transform 3s ease-out";
-//   wheel.style.transform = `rotate(${randomDeg}deg)`; // Apply rotation
-// });
+function getResultsAtTop(spinnerElement) {
+  if (!spinnerElement) return null;
+  
+  const rect = spinnerElement.getBoundingClientRect();
+  const centerX = rect.left + rect.width / 2;
+  const topY = rect.top + (.05 * rect.height); // the arrow is 4%, so we use 5%
+  const element = document.elementFromPoint(centerX, topY);
+  if (element?.parentElement?.parentElement !== spinnerElement) return null;
+  return element?.textContent?.trim() || null;
+}
+
+function wheelOfFortune(node) {
+  if (!node) return;
+
+  const spin = node.querySelector('button');
+  const wheel = node.querySelector('ul');
+  let animation;
+  let previousEndDegree = 0;
+
+  spin.addEventListener('click', () => {
+    if (animation) {
+      animation.cancel(); // Reset the animation if it already exists
+    }
+
+    const randomAdditionalDegrees = Math.random() * 360 + 1800;
+    const newEndDegree = previousEndDegree + randomAdditionalDegrees;
+
+    animation = wheel.animate([
+      { transform: `rotate(${previousEndDegree}deg)` },
+      { transform: `rotate(${newEndDegree}deg)` }
+    ], {
+      duration: 4000,
+      direction: 'normal',
+      easing: 'cubic-bezier(0.440, -0.205, 0.000, 1.130)',
+      fill: 'forwards',
+      iterations: 1
+    });
+
+    previousEndDegree = newEndDegree;
+    animation.onfinish = () => {
+      const currentValue = getResultsAtTop(node);
+      spin.textContent = currentValue;
+    };
+  });
+}
+
+// Usage
+document.querySelectorAll('.main-container').forEach(el => wheelOfFortune(el))
+// wheelOfFortune('.ui-wheel-of-fortune');
